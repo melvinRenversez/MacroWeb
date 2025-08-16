3c5591e4b93b1ad5ed9c192f1de610950e0f1251
melvinRenversez <melvinrenversez003@gmail.com>
2025-08-16 22:21:07 +0200

    int

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import json
+
+# Max 45 button
 
 
 HOST = "0.0.0.0"
@@ -10,6 +13,26 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+
+
+
+
+
+
+
+@app.route("/api/getAircraftName")
+def getAicraftName():
+
+    with open("data.json", "r", encoding="utf-8") as f:
+        data = json.load(f) 
+
+    print(data)
+    
+    aicraftName = list(data.keys())
+    
+    return aicraftName
 
 
 if __name__ == "__main__":
